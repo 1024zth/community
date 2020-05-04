4af2647fdc1a2e01e4debb329cad96ae73deb20a
Wumo <1152475833@qq.com>
2020-05-04 22:05:04 +0800

    day01

--- src/main/java/com/wumo/community/controller/AuthorizeController.java
+++ src/main/java/com/wumo/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.wumo.community.controller;
 
 import com.wumo.community.dto.AccessTokenDTO;
 import com.wumo.community.dto.GithubUser;
+import com.wumo.community.mapper.UserMapper;
+import com.wumo.community.model.User;
 import com.wumo.community.provider.GithubProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("Iv1.8c7ee036fde0b5f2")
     private String clientId;
@@ -27,7 +35,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -36,10 +45,26 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            //登录成功 写cookie和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccount_id(String.valueOf(githubUser.getId()));
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            userMapper.insert(user);
 
-        System.out.println(user.getName());
-        return "index";
+            request.getSession().setAttribute("githubUser",githubUser);
+            return "redirect:/";
+
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
+
+//        System.out.println(githubUser.getName());
     }
 
 
--- src/main/java/com/wumo/community/dto/AccessTokenDTO.java
+++ src/main/java/com/wumo/community/dto/AccessTokenDTO.java
@@ -1,8 +1,5 @@
 package com.wumo.community.dto;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
 public class AccessTokenDTO {
     private String client_id;
     private String client_secret;
